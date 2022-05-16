654e884dba50aa5f182c8253416d1ef17edd96e1
Moe Kurdi <kurdi1128@gmail.com>
2022-05-15 23:30:55 -0400

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -36,24 +36,7 @@ import ErrorPage from '../views/404'
 import AdvanceTabPage from '../views/AdvanceTabPage'
 import BrandCarouselPage from '../views/BrandCarouselPage'
 import AdvancePricingPage from '../views/AdvancePricingPage'
-import BusinessConsulting from '../views/home-pages/BusinessConsulting'
-import BusinessConsulting2 from '../views/home-pages/BusinessConsulting2'
-import Corporate from '../views/home-pages/Corporate'
-import Business from '../views/home-pages/Business'
-import DigitalAgency from '../views/home-pages/DigitalAgency'
-import Finance from '../views/home-pages/Finance'
-import Company from '../views/home-pages/Company'
-import MarketAgency from '../views/home-pages/MarketAgency'
-import TravelAgency from '../views/home-pages/TravelAgency'
-import Consulting from '../views/home-pages/Consulting'
-import SeoAgency from '../views/home-pages/SeoAgency'
-import PersonalPortfolio from '../views/home-pages/PersonalPortfolio'
-import EventConference from '../views/home-pages/EventConference'
-import CreativePortfolio from '../views/home-pages/CreativePortfolio'
-import Freelancer from '../views/home-pages/Freelancer'
-import InternationalConsulting from '../views/home-pages/InternationalConsulting'
-import Startup from '../views/home-pages/Startup'
-import WebAgency from '../views/home-pages/WebAgency'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -61,7 +44,7 @@ const routes = [
     {
         path: '/',
         name: 'Demo',
-        component: Demo,
+        component: Home,
         meta: {
             title: 'Doob || Doob Business and Consulting Bootstrap5 Template',
         },
@@ -362,150 +345,6 @@ const routes = [
             title: 'Blog Details || Doob Business and Consulting Bootstrap5 Template',
         },
     },
-    {
-        path: '/business-consulting',
-        name: 'Business Consulting',
-        component: BusinessConsulting,
-        meta: {
-            title: 'Business Consulting || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/business-consulting-2',
-        name: 'Business Consulting 2',
-        component: BusinessConsulting2,
-        meta: {
-            title: 'Business Consulting 2 || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/corporate',
-        name: 'Corporate',
-        component: Corporate,
-        meta: {
-            title: 'Corporate || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/business',
-        name: 'Business',
-        component: Business,
-        meta: {
-            title: 'Business || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/digital-agency',
-        name: 'Digital Agency',
-        component: DigitalAgency,
-        meta: {
-            title: 'Digital Agency || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/finance',
-        name: 'Finance',
-        component: Finance,
-        meta: {
-            title: 'Finance || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/company',
-        name: 'Company',
-        component: Company,
-        meta: {
-            title: 'Company || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/marketing-agency',
-        name: 'Marketing Agency',
-        component: MarketAgency,
-        meta: {
-            title: 'Marketing Agency || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/travel-agency',
-        name: 'Travel Agency',
-        component: TravelAgency,
-        meta: {
-            title: 'Travel Agency || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/consulting',
-        name: 'Consulting',
-        component: Consulting,
-        meta: {
-            title: 'Consulting || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/seo-agency',
-        name: 'SEO Agency',
-        component: SeoAgency,
-        meta: {
-            title: 'SEO Agency || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/personal-portfolio',
-        name: 'Personal Portfolio',
-        component: PersonalPortfolio,
-        meta: {
-            title: 'Personal Portfolio || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/event-conference',
-        name: 'Event Conference',
-        component: EventConference,
-        meta: {
-            title: 'Event Conference || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/creative-portfolio',
-        name: 'Creative Portfolio',
-        component: CreativePortfolio,
-        meta: {
-            title: 'Creative Portfolio || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/freelancer',
-        name: 'Freelancer',
-        component: Freelancer,
-        meta: {
-            title: 'Freelancer || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/international-consulting',
-        name: 'International Consulting',
-        component: InternationalConsulting,
-        meta: {
-            title: 'International Consulting || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/startup',
-        name: 'Startup',
-        component: Startup,
-        meta: {
-            title: 'Startup || Doob Business and Consulting Bootstrap5 Template',
-        },
-    },
-    {
-        path: '/web-agency',
-        name: 'Web Agency',
-        component: WebAgency,
-        meta: {
-            title: 'Web Agency || Doob Business and Consulting Bootstrap5 Template',
-        },
-    }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,19 +7,44 @@
                     <div class="row align-items-center">
                         <div class="col-lg-12">
                             <div class="inner text-center">
-                                <span class="subtitle theme-gradient" data-aos="fade-up" data-aos-delay="100">Hello! This is WebAgency</span>
-                                <h1 class="title" data-aos="fade-up" data-aos-delay="150">
-                                    Our Web <br /> Agecny Plan.
+                                <span
+                                    class="subtitle theme-gradient"
+                                    data-aos="fade-up"
+                                    data-aos-delay="100"
+                                    >Hello! This is WebAgency</span
+                                >
+                                <h1
+                                    class="title"
+                                    data-aos="fade-up"
+                                    data-aos-delay="150"
+                                >
+                                    Our Web <br />
+                                    Agecny Plan.
                                 </h1>
-                                <p class="description" data-aos="fade-up" data-aos-delay="200">
-                                    We help our clients succeed by creating brand identities, <br /> digital experiences, and print materials.
+                                <p
+                                    class="description"
+                                    data-aos="fade-up"
+                                    data-aos-delay="200"
+                                >
+                                    We help our clients succeed by creating
+                                    brand identities, <br />
+                                    digital experiences, and print materials.
                                 </p>
-                                <div class="button-group mt--30" data-aos="fade-up" data-aos-delay="250">
-                                    <router-link class="btn-default btn-large round"
-                                       to="#">
+                                <div
+                                    class="button-group mt--30"
+                                    data-aos="fade-up"
+                                    data-aos-delay="250"
+                                >
+                                    <router-link
+                                        class="btn-default btn-large round"
+                                        to="#"
+                                    >
                                         Explore Now
                                     </router-link>
-                                    <router-link class="btn-default btn-border btn-large round" to="/contact">
+                                    <router-link
+                                        class="btn-default btn-border btn-large round"
+                                        to="/contact"
+                                    >
                                         Contact Us
                                     </router-link>
                                 </div>
@@ -37,7 +62,11 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="thumbnail" data-aos="fade-up">
-                            <img class="radius w-100" src="../../assets/images/bg/bg-image-21.jpg" alt="Images"/>
+                            <img
+                                class="radius w-100"
+                                src="../assets/images/bg/bg-image-21.jpg"
+                                alt="Images"
+                            />
                         </div>
                         <div class="rwt-counterup-area ptb--60">
                             <div class="col-lg-12">
@@ -55,7 +84,7 @@
         </div>
         <!-- End Counterup Area -->
 
-        <Separator/>
+        <Separator />
 
         <!-- Start Service Area -->
         <div class="service-area rn-section-gapTop">
@@ -63,12 +92,19 @@
                 <div class="row align-items-center">
                     <div class="col-lg-6">
                         <div class="content" data-aos="fade-up">
-                            <h3 class="title">We are creative digital agency working for our company brands.</h3>
+                            <h3 class="title">
+                                We are creative digital agency working for our
+                                company brands.
+                            </h3>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <p class="mb--10" data-aos="fade-up">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quis. Ullam accusantium dignissimos repellendus nemo fugiat numquam, nisi odio adipisci. Veniam neque itaque expedita officiis rem ipsa! Ratione, rem reiciendis?
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quod, quis. Ullam accusantium dignissimos
+                            repellendus nemo fugiat numquam, nisi odio adipisci.
+                            Veniam neque itaque expedita officiis rem ipsa!
+                            Ratione, rem reiciendis?
                         </p>
                         <div class="readmore-btn" data-aos="fade-up">
                             <router-link class="btn-read-more" to="#">
@@ -103,7 +139,9 @@
         </div>
         <!-- End Service Area -->
 
-        <AboutFour :image="require(`@/assets/images/about/contact-image.jpg`)"/>
+        <AboutFour
+            :image="require(`@/assets/images/about/contact-image.jpg`)"
+        />
 
         <!-- Start Pricing Area -->
         <div class="rwt-pricingtable-area rn-section-gapBottom">
@@ -120,7 +158,7 @@
                 </div>
                 <div class="row mt--30">
                     <div class="col-lg-8 offset-lg-2">
-                        <AdvancePricing/>
+                        <AdvancePricing />
                     </div>
                 </div>
             </div>
@@ -142,7 +180,7 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-12 mt--40">
-                        <Brand :brand-list="brandList"/>
+                        <Brand :brand-list="brandList" />
                     </div>
                 </div>
             </div>
@@ -152,64 +190,73 @@
 </template>
 
 <script>
-    import Layout from "../../components/common/Layout";
-    import Separator from "../../components/elements/separator/Separator";
-    import SectionTitle from "../../components/elements/sectionTitle/SectionTitle";
-    import AboutFour from "../../components/elements/about/AboutFour";
-    import AdvancePricing from "../../components/elements/pricing/AdvancePricing";
-    import Brand from "../../components/elements/brand/Brand";
-    import Counter from "../../components/elements/counterUp/Counter";
-    import ServiceOne from "../../components/elements/service/ServiceOne";
-    export default {
-        name: 'WebAgency',
-        components: {ServiceOne, Counter, Brand, AdvancePricing, AboutFour, SectionTitle, Separator, Layout},
-        data() {
-            return {
-                brandList: [
-                    {
-                        image: require(`@/assets/images/brand/brand-01.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-02.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-03.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-04.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-05.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-06.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-07.png`)
-                    },
-                    {
-                        image: require(`@/assets/images/brand/brand-08.png`)
-                    }
-                ],
-                counterData: [
-                    {
-                        number: 199,
-                        title: 'Happy Clients',
-                    },
-                    {
-                        number: 575,
-                        title: 'Employees',
-                    },
-                    {
-                        number: 69,
-                        title: 'Useful Programs',
-                    },
-                    {
-                        number: 500,
-                        title: 'Useful Programs',
-                    }
-                ]
-            }
+import Layout from '../components/common/Layout'
+import Separator from '../components/elements/separator/Separator'
+import SectionTitle from '../components/elements/sectionTitle/SectionTitle'
+import AboutFour from '../components/elements/about/AboutFour'
+import AdvancePricing from '../components/elements/pricing/AdvancePricing'
+import Brand from '../components/elements/brand/Brand'
+import Counter from '../components/elements/counterUp/Counter'
+import ServiceOne from '../components/elements/service/ServiceOne'
+export default {
+    name: 'Home',
+    components: {
+        ServiceOne,
+        Counter,
+        Brand,
+        AdvancePricing,
+        AboutFour,
+        SectionTitle,
+        Separator,
+        Layout,
+    },
+    data() {
+        return {
+            brandList: [
+                {
+                    image: require(`@/assets/images/brand/brand-01.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-02.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-03.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-04.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-05.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-06.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-07.png`),
+                },
+                {
+                    image: require(`@/assets/images/brand/brand-08.png`),
+                },
+            ],
+            counterData: [
+                {
+                    number: 199,
+                    title: 'Happy Clients',
+                },
+                {
+                    number: 575,
+                    title: 'Employees',
+                },
+                {
+                    number: 69,
+                    title: 'Useful Programs',
+                },
+                {
+                    number: 500,
+                    title: 'Useful Programs',
+                },
+            ],
         }
-    }
+    },
+}
 </script>
