079e1631b0c16ddb1f764e02b69b46b865e37f96
Moe Kurdi <kurdi1128@gmail.com>
2022-05-16 22:26:31 -0400

    form

--- src/components/elements/contact/ContactForm.vue
+++ src/components/elements/contact/ContactForm.vue
@@ -1,5 +1,11 @@
 <template>
-    <form class="contact-form-1" action="" @submit="sendEmail">
+    <form
+        netlify
+        name="contact"
+        class="contact-form-1"
+        action=""
+        @submit="sendEmail"
+    >
         <div class="form-group">
             <input
                 type="text"
@@ -27,45 +33,38 @@
             />
         </div>
 
-
         <div class="form-group">
-            <input
-                type="text"
-                name="subject"
-                placeholder="Subject"
-                required
-            />
+            <input type="text" name="subject" placeholder="Subject" required />
         </div>
 
         <div class="form-group">
-                <textarea
-                    name="message"
-                    placeholder="Your Message"
-                    required>
-                </textarea>
+            <textarea name="message" placeholder="Your Message" required>
+            </textarea>
         </div>
 
         <div class="form-group">
-            <button class="btn-default btn-large">Submit Now</button>
+            <button type="submit" class="btn-default btn-large">
+                Submit Now
+            </button>
         </div>
 
-        <div class="form-group" v-html="showResult ? result : null"/>
+        <div class="form-group" v-html="showResult ? result : null" />
     </form>
 </template>
 
 <script>
-    export default {
-        name: 'ContactForm',
-        data() {
-            return {
-                showResult: false,
-                result: `<p class="success-message">Your Message has been successfully sent. I will contact you soon.</p>`
-            }
-        },
-        methods: {
-            sendEmail() {
-                this.showResult = true
-            }
+export default {
+    name: 'ContactForm',
+    data() {
+        return {
+            showResult: false,
+            result: `<p class="success-message">Your Message has been successfully sent. I will contact you soon.</p>`,
         }
-    }
+    },
+    methods: {
+        sendEmail() {
+            this.showResult = true
+        },
+    },
+}
 </script>
--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -16,40 +16,42 @@
                     <div class="col-lg-12">
                         <div class="rn-contact-address mt_dec--30">
                             <div class="row">
-                                <div class="col-lg-4 col-md-6 col-12">
+                                <div class="col-lg-6 col-md-6 col-12">
                                     <div class="rn-address">
                                         <div class="icon">
-                                            <Icon name="headphones" :size="40"/>
+                                            <Icon
+                                                name="headphones"
+                                                :size="40"
+                                            />
                                         </div>
                                         <div class="inner">
-                                            <h4 class="title">Contact Phone Number</h4>
-                                            <p><a href="tel:+444 555 666 777">+444 555 666 777</a></p>
-                                            <p><a href="tel:+222 222 222 333">+222 222 222 333</a></p>
+                                            <h4 class="title">
+                                                Contact Phone Number
+                                            </h4>
+                                            <p>
+                                                <a href="tel:+1 502 276 5950"
+                                                    >+1 502 276 5950</a
+                                                >
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="col-lg-4 col-md-6 col-12">
+                                <div class="col-lg-6 col-md-6 col-12">
                                     <div class="rn-address">
                                         <div class="icon">
-                                            <Icon name="mail" :size="40"/>
+                                            <Icon name="mail" :size="40" />
                                         </div>
                                         <div class="inner">
-                                            <h4 class="title">Our Email Address</h4>
-                                            <p><a href="mailto:admin@gmail.com">admin@gmail.com</a></p>
-                                            <p><a href="mailto:example@gmail.com">example@gmail.com</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-4 col-md-6 col-12">
-                                    <div class="rn-address">
-                                        <div class="icon">
-                                            <Icon name="map-pin" :size="40"/>
-                                        </div>
-                                        <div class="inner">
-                                            <h4 class="title">Our Location</h4>
-                                            <p>5678 Bangla Main Road, cities 580 <br /> GBnagla, example 54786</p>
+                                            <h4 class="title">
+                                                Our Email Address
+                                            </h4>
+                                            <p>
+                                                <a
+                                                    href="mailto:lunarlabsmedia@gmail.com"
+                                                    >lunarlabsmedia@gmail.com</a
+                                                >
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -60,10 +62,10 @@
 
                 <div class="row mt--40 row--15">
                     <div class="col-lg-7">
-                        <ContactForm/>
+                        <ContactForm />
                     </div>
                     <div class="col-lg-5 mt_md--30 mt_sm--30">
-                        <GoogleMap/>
+                        <GoogleMap />
                     </div>
                 </div>
             </div>
@@ -73,15 +75,22 @@
 </template>
 
 <script>
-    import Layout from '../components/common/Layout'
-    import Breadcrumb from '../components/elements/breadcrumb/Breadcrumb'
-    import SectionTitle from '../components/elements/sectionTitle/SectionTitle'
-    import Icon from "../components/icon/Icon";
-    import GoogleMap from "../components/elements/contact/GoogleMap";
-    import ContactForm from "../components/elements/contact/ContactForm";
+import Layout from '../components/common/Layout'
+import Breadcrumb from '../components/elements/breadcrumb/Breadcrumb'
+import SectionTitle from '../components/elements/sectionTitle/SectionTitle'
+import Icon from '../components/icon/Icon'
+import GoogleMap from '../components/elements/contact/GoogleMap'
+import ContactForm from '../components/elements/contact/ContactForm'
 
-    export default {
-        name: 'ContactPage',
-        components: {ContactForm, GoogleMap, Icon, SectionTitle, Breadcrumb, Layout}
-    }
+export default {
+    name: 'ContactPage',
+    components: {
+        ContactForm,
+        GoogleMap,
+        Icon,
+        SectionTitle,
+        Breadcrumb,
+        Layout,
+    },
+}
 </script>
